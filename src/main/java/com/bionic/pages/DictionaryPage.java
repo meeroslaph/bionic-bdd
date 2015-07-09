a09ba498d05706ba47aca5158c359dcfb1cac092
meeroslaph <m@meeroslaph.name>
2015-07-09 23:28:10 +0300

    Serenity BDD packages are used instead of Thucydides for some classes.

--- src/main/java/com/bionic/pages/DictionaryPage.java
+++ src/main/java/com/bionic/pages/DictionaryPage.java
@@ -1,10 +1,10 @@
 package com.bionic.pages;
 
 import ch.lambdaj.function.convert.Converter;
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
-import net.thucydides.core.annotations.findby.FindBy;
-import net.thucydides.core.pages.PageObject;
-import net.thucydides.core.pages.WebElementFacade;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
