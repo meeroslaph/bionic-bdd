9c1a8aba4ec2cecf9381cdbbeba3e17f5201a8d3
meeroslaph <m@meeroslaph.name>
2015-07-12 18:23:08 +0300

    Search for phone test updates.

--- src/main/java/com/bionic/pages/SearchResultPage.java
+++ src/main/java/com/bionic/pages/SearchResultPage.java
@@ -10,15 +10,14 @@ import java.util.List;
 public class SearchResultPage extends PageObject {
     @FindBy(css = ".search-result > *")
     private List<WebElementFacade> resultProducts;
-
-    private By productImage = By.className("image");
+    private By productName = By.cssSelector(".title>a");
 
     public String getProductName(int position) {
-        return resultProducts.get(position).getText();
+        return resultProducts.get(position).findElement(productName).getText();
     }
 
     public ProductPage openProduct(int position) {
-        resultProducts.get(position).findElement(productImage).click();
+        resultProducts.get(position).findElement(productName).click();
         return new ProductPage();
     }
 }
